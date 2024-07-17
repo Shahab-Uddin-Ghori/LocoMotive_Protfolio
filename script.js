33b7b7a1108e39b61a2b4b2eb7494515d2fd7069
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-07-17 19:10:11 +0500

    video gsap locomotive added

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+const loading = () =>{
+    let tl = gsap.timeline()
+
+
+tl.to("#yellow1",{
+    top: "-100%",
+    delay:.5,
+    duration: 0.5,
+    ease: "expo.out"
+})
+
+tl.from("#yellow2",{
+    top: "100%",
+    delay:3,
+    duration: 0.5,
+    ease: "expo.out" 
+},"anima")
+tl.to("#loader h1",{
+    delay: 0.1,
+    duration: 0.5,
+    color: "black"
+},"anima")
+tl.to("#loader",{
+    display: "none "
+})
+
+}
+loading()
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
-    smooth: true
+    smooth: true,
+    lerp: 0.05
 });
