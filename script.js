f852221a6da671f98783622b4e5ac28ea9c14cba
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-07-17 22:22:13 +0500

    page 2 setup

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-const loading = () =>{
-    let tl = gsap.timeline()
+const loading = () => {
+  let tl = gsap.timeline();
 
-
-tl.to("#yellow1",{
+  tl.to("#yellow1", {
     top: "-100%",
-    delay:.5,
+    delay: 0.5,
     duration: 0.5,
-    ease: "expo.out"
-})
+    ease: "expo.out",
+  });
 
-tl.from("#yellow2",{
-    top: "100%",
-    delay:3,
-    duration: 0.5,
-    ease: "expo.out" 
-},"anima")
-tl.to("#loader h1",{
-    delay: 0.1,
-    duration: 0.5,
-    color: "black"
-},"anima")
-tl.to("#loader",{
-    display: "none "
-})
-
-}
-loading()
+  tl.from(
+    "#yellow2",
+    {
+      top: "100%",
+      delay: 3,
+      duration: 0.5,
+      ease: "expo.out",
+    },
+    "anima"
+  );
+  tl.to(
+    "#loader h1",
+    {
+      delay: 0.1,
+      duration: 0.5,
+      color: "black",
+    },
+    "anima"
+  );
+  tl.to("#loader", {
+    display: "none ",
+  });
+};
+loading();
 
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true,
-    lerp: 0.08
+  el: document.querySelector("#main"),
+  smooth: true,
+  lerp: 0.08,
 });
